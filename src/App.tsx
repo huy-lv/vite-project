5f73b39d98128da33dbd0caa0e3d41e182cb26d9
huy.le3 <huy.le3@mservice.com.vn>
2024-05-25 10:58:35 +0700

    add remote-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { RemoteComponent } from "./RemoteComponent";
+
+const url = "https://raw.githubusercontent.com/Paciolan/remote-component/master/examples/remote-components/HelloWorld.js"; // prettier-ignore
+
+const HelloWorld = (props: any) => <RemoteComponent url={url} {...props} />;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -17,6 +22,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <HelloWorld name="Paciolan" />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -29,7 +35,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
